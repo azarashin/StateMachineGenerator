242ab74ee0c1b42f7a896bd492f49aa5de777c76
azarashin <azarashin1979@gmail.com>
2024-04-07 14:59:32 +0900

    added source code for test expection.

---
+++ test/data/cs/test01/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+IControllee controllee = new ConsoleOutControllee();
+StateController stateController = new StateController(controllee);
+
+stateController.ExecCommand2();
+stateController.ExecCommand2();
+stateController.ExecCommand1();
+stateController.ExecCommand1();
+stateController.ExecCommand3();
+stateController.ExecCommand3();
+stateController.ExecCommand1();
+stateController.ExecCommand1();
