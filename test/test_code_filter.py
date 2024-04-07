ba84d088f8c022c8b294815e3938b325b38a21b8
azarashin <azarashin1979@gmail.com>
2024-04-07 13:50:30 +0900

    bug fixed(test code).

--- test/test_code_filter.py
+++ test/test_code_filter.py
@@ -6,36 +6,34 @@ def test_01():
     assert filter_code(source) == expected
 
 def test_02():
-    source = 'line1\
-\
-line2\
-'
-    expected = 'line1\
-\
-line2\
-'
+    source = """line1
+
+line2
+"""
+    expected = """line1
+line2"""
     assert filter_code(source) == expected
 
 def test_03():
-    source = 'line1\
-\
-line2\
-'
-    expected = 'line1\
-line2\
-'
+    source = """
+line1
+
+line2
+
+"""
+    expected = """line1
+line2"""
     assert filter_code(source) == expected
 
 
 def test_04():
-    source = '\
-\
-line1\
-\
-line2\
-\
-'
-    expected = 'line1\
-line2\
-'
+    source = """
+
+  line1
+    
+    line2
+
+"""
+    expected = """  line1
+    line2"""
     assert filter_code(source) == expected
